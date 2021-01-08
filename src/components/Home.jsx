7c04651f646c9a0cb0d32ac378badf832f82624b
muhmmqibal <muhammadiqbal130996@gmail.com>
2021-01-08 16:33:36 +0700

    add jumbotron & card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Jumbotron from './jumbotron'
+import LearnCard from './card'
+
 
 function Home() {
     return(
         <div>
+            <Jumbotron />
+            <Container>
             <h1>Ini Home</h1>
+            <LearnCard />
+            </Container>
+            
+            <Jumbotron />
+
         </div>
     )
 }
