30e9cff2736a613f93c418dd09b37c9d21d1be1d
muhmmqibal <muhammadiqbal130996@gmail.com>
2021-01-08 16:50:25 +0700

    pull iqbal

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Main from './components/main';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ src/page/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SignUp() {
+    return(
+        <div>
+            <h1>Ini SignUp</h1>
+        </div>
+    )
+}
+
+export default SignUp;
